d43e9523c07a8160c5835ef8878ccb81e32164fa
1320503154 <1320503154@qq.com>
2024-09-28 14:11:45 +0800

    还原next设置

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	// 配置输出为静态导出
+	output: "export",
+	// 配置导出路径映射
+	exportPathMap: async function (
+		defaultPathMap,
+		{ dev, dir, outDir, distDir, buildId }
+	) {
+		return {
+			// 配置根路径
+			"/": { page: "/" },
+			// 其他页面的配置
+		};
+	},
+};
 
 export default nextConfig;
