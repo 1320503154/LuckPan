63917f9e5d336f12b20b5f17fcb9164aa12e4416
1320503154 <1320503154@qq.com>
2024-09-28 14:11:53 +0800

    提交到github

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	// 配置输出为静态导出
-	output: "export",
-	// 配置导出路径映射
-	exportPathMap: async function (
-		defaultPathMap,
-		{ dev, dir, outDir, distDir, buildId }
-	) {
-		return {
-			// 配置根路径
-			"/": { page: "/" },
-			// 其他页面的配置
-		};
-	},
-};
+const nextConfig = {};
 
 export default nextConfig;
